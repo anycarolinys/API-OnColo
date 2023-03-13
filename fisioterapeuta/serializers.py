b8e22bdadf7c32629bdb69efd680db5ee18d0faa
Arthur Lima <61024534+arthurm170110@users.noreply.github.com>
2023-03-13 18:51:24 -0300

    Update serializers.py

--- fisioterapeuta/serializers.py
+++ fisioterapeuta/serializers.py
@@ -6,12 +6,6 @@ class FisioterapeutaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Fisioterapeuta
         fields = '__all__'
-
-    def to_internal_value(self, data):
-        data._mutable = True
-        data['nome'] = " ".join(data['nome'].split())
-
-        return super(FisioterapeutaSerializer, self).to_internal_value(data)
     
     def validate(self, data):
         matricula_valida(data['matricula'])
