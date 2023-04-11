49c7bfa42c0469517178d0bd69fd4263de970d49
Rafael Sousa <66565227+Rafaavinii@users.noreply.github.com>
2023-04-11 13:52:16 -0300

    Add files via upload

--- autenticacao/urls.py
+++ autenticacao/urls.py
@@ -1,8 +1,7 @@
-from django.urls import path, include
+from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from autenticacao.views import LoginViewSet
-from rest_framework.routers import DefaultRouter
 
-router = DefaultRouter()
-router.register(r'', LoginViewSet, basename='login')
-urlpatterns = router.urls
+urlpatterns = [
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
--- autenticacao/views.py
+++ autenticacao/views.py
@@ -1,16 +1,16 @@
-from rest_framework import viewsets
-from django.http import HttpResponse
-from django.shortcuts import render, redirect
-from rest_framework.authtoken.models import Token
-from django.contrib.auth import authenticate, login
+# from rest_framework import viewsets
+# from django.http import HttpResponse
+# from django.shortcuts import render, redirect
+# from rest_framework.authtoken.models import Token
+# from django.contrib.auth import authenticate, login
 
-class LoginViewSet(viewsets.ViewSet):
+# class LoginViewSet(viewsets.ViewSet):
     
-    def create(self, request):
-        email = request.data.get('email')
-        password = request.data.get('password')
+#     def create(self, request):
+#         email = request.data.get('email')
+#         password = request.data.get('password')
         
-        user = authenticate(request, email=email, password=password)
+#         user = authenticate(request, email=email, password=password)
         
-        login(request, user)
-        return redirect('/fisioterapeuta/')
+#         login(request, user)
+#         return redirect('/fisioterapeuta/')
