80f6e763cd49378420cdd46ddc8333e2a47bbf68
Rafael Sousa <66565227+Rafaavinii@users.noreply.github.com>
2023-04-11 19:41:45 -0300

    Update urls.py

--- projetoinca/urls.py
+++ projetoinca/urls.py
@@ -1,6 +1,10 @@
 from django.urls import include, path
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
+    path("schema/", SpectacularAPIView.as_view(), name="schema"),
+    path('schema/docs/', SpectacularSwaggerView.as_view(url_name='schema')),
+    
     path('auth/', include('autenticacao.urls')),
 
     path('paciente/', include('paciente.urls')),
