a14ad5888ade338a48125bfbcd1c3f0daf7e9dbe
Rafael Sousa <66565227+Rafaavinii@users.noreply.github.com>
2023-04-11 13:52:52 -0300

    Update views.py

--- instituicao/views.py
+++ instituicao/views.py
@@ -2,6 +2,8 @@ from .models import Instituicao
 from .serializers import InstituicaoSerializer
 from rest_framework import viewsets, filters
 from django_filters.rest_framework import DjangoFilterBackend
+from useradmin.models import UserInstituicao
+from rest_framework.response import Response
 
 
 class InstituicaoViewSet(viewsets.ModelViewSet):
@@ -10,3 +12,18 @@ class InstituicaoViewSet(viewsets.ModelViewSet):
     filter_backends = [DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter]
     ordering_fields = ['nome']
     search_fields = ['nome', 'cnpj']
+
+    def create(self, request, *args, **kwargs):
+        nome = request.data.get('nome')
+        cnpj = request.data.get('cnpj')
+        email = request.data.get('email')
+        senha = request.data.get('senha')
+
+        instituicao = Instituicao.objects.create(nome=nome, cnpj=cnpj, email=email, senha=senha)
+        instituicao.save()
+
+        id = instituicao.id
+        UserInstituicao.objects.create_user(username=email, email=email, password=senha, instituicao_id=id)
+
+        return Response({'detail': 'Cadastrado com sucesso'})
+
