57ad84b329f1a5a92e09a06ff5bb8d4e9b043070
anycarolinys <caroliny663@gmail.com>
2023-04-11 13:03:52 -0300

    Removendo listagem AFM, AGPG e AICS

--- formularios/views.py
+++ formularios/views.py
@@ -7,9 +7,9 @@ from formularios.formFSFI.models import FormFSFI
 from formularios.formKHQ.models import FormKHQ
 from formularios.formQVC.models import FormQVC
 from formularios.formQVCC.models import FormQVCC
-from formularios.formAFM.models import FormAFM
-from formularios.formAGPG.models import FormAGPG
-from formularios.formAICS.models import FormAICS
+# from formularios.formAFM.models import FormAFM
+# from formularios.formAGPG.models import FormAGPG
+# from formularios.formAICS.models import FormAICS
 from paciente.models import Paciente
 
 from formularios.formAPR.serializers import FormAPRSerializer
@@ -18,9 +18,9 @@ from formularios.formFSFI.serializers import FormFSFISerializer
 from formularios.formKHQ.serializers import FormKHQSerializer
 from formularios.formQVC.serializers import FormQVCSerializer
 from formularios.formQVCC.serializers import FormQVCCSerializer
-from formularios.formAFM.serializers import FormAFMSerializer
-from formularios.formAGPG.serializers import FormAGPGSerializer
-from formularios.formAICS.serializers import FormAICSSerializer
+# from formularios.formAFM.serializers import FormAFMSerializer
+# from formularios.formAGPG.serializers import FormAGPGSerializer
+# from formularios.formAICS.serializers import FormAICSSerializer
 
 class FormulariosView(APIView):
     def get(self, request):
@@ -40,9 +40,9 @@ class FormulariosView(APIView):
         serializer4 = FormKHQSerializer(queryset4, many=True)
         serializer5 = FormQVCSerializer(queryset5, many=True)
         serializer6 = FormQVCCSerializer(queryset6, many=True)
-        serializer7 = FormAFMSerializer(queryset7, many=True)
-        serializer8 = FormAGPGSerializer(queryset8, many=True)
-        serializer9 = FormAICSSerializer(queryset9, many=True)
+        # serializer7 = FormAFMSerializer(queryset7, many=True)
+        # serializer8 = FormAGPGSerializer(queryset8, many=True)
+        # serializer9 = FormAICSSerializer(queryset9, many=True)
 
         data = {
             'FormAPR': serializer1.data,
@@ -51,9 +51,9 @@ class FormulariosView(APIView):
             'FormKHQ': serializer4.data,
             'FormQVC': serializer5.data,
             'FormQVCC': serializer6.data,
-            'FormAFM' : serializer7.data,
-            'FormAGPG' : serializer8.data,
-            'FormAICS' : serializer9.data,
+            # 'FormAFM' : serializer7.data,
+            # 'FormAGPG' : serializer8.data,
+            # 'FormAICS' : serializer9.data,
         }
 
         return Response(data)
@@ -69,9 +69,9 @@ class FormulariosPorPacienteView(APIView):
         formkhq = []
         formqvc = []
         formqvcc = []
-        formafm = []
-        formagpg = []
-        formaics = []
+        # formafm = []
+        # formagpg = []
+        # formaics = []
 
         for consulta in consultas:
             formapr = formapr + [formulario for formulario in consulta.formapr_set.all()]
@@ -80,9 +80,9 @@ class FormulariosPorPacienteView(APIView):
             formkhq = formkhq + [formulario for formulario in consulta.formkhq_set.all()]
             formqvc = formqvc + [formulario for formulario in consulta.formqvc_set.all()]
             formqvcc = formqvcc + [formulario for formulario in consulta.formqvcc_set.all()]
-            formafm = formafm + [formulario for formulario in consulta.formafm_set.all()]
-            formagpg = formagpg + [formulario for formulario in consulta.formagpg_set.all()]
-            formaics = formaics + [formulario for formulario in consulta.formaics_set.all()]
+            # formafm = formafm + [formulario for formulario in consulta.formafm_set.all()]
+            # formagpg = formagpg + [formulario for formulario in consulta.formagpg_set.all()]
+            # formaics = formaics + [formulario for formulario in consulta.formaics_set.all()]
         
         serializer1 = FormAPRSerializer(formapr, many=True)
         serializer2 = FormFIQLSerializer(formfiql, many=True)
@@ -90,9 +90,9 @@ class FormulariosPorPacienteView(APIView):
         serializer4 = FormKHQSerializer(formkhq, many=True)
         serializer5 = FormQVCSerializer(formqvc, many=True)
         serializer6 = FormQVCCSerializer(formqvcc, many=True)
-        serializer7 = FormAFMSerializer(formqvcc, many=True)
-        serializer8 = FormAGPGSerializer(formqvcc, many=True)
-        serializer9 = FormAICSSerializer(formqvcc, many=True)
+        # serializer7 = FormAFMSerializer(formafm, many=True)
+        # serializer8 = FormAGPGSerializer(formagpg, many=True)
+        # serializer9 = FormAICSSerializer(formaics, many=True)
 
         data = {
             'FormAPR': serializer1.data,
@@ -101,9 +101,9 @@ class FormulariosPorPacienteView(APIView):
             'FormKHQ': serializer4.data,
             'FormQVC': serializer5.data,
             'FormQVCC': serializer6.data,
-            'FormAFM' : serializer7.data,
-            'FormAGPG' : serializer8.data,
-            'FormAICS' : serializer9.data,
+            # 'FormAFM' : serializer7.data,
+            # 'FormAGPG' : serializer8.data,
+            # 'FormAICS' : serializer9.data,
         }
 
         return Response(data)
